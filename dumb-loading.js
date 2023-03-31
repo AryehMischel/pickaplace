d2a8239fc7abeef587b7f39a2f833f187ef03683
Aryeh Mischel <asmischel5@gmail.com>
2023-03-31 19:17:49 -0400

    removed logs
    removed console logs
    changed a-frame version in index.html

--- dumb-loading.js
+++ dumb-loading.js
@@ -9,67 +9,6 @@ function loadNext(){
 }
 
 
-scene.addEventListener("loaded", ()=> loadNext() );
+scene.addEventListener("loaded", ()=> loadNext());
 
 
-/*
-//shhh lets not talk about this...
-//dumb way to cache images...
-AFRAME.registerComponent("dumb-loading", {
-    init: function () {
-
-        let revisiting = false;
-        window.onbeforeunload = function (e) {
-            if (revisiting) {
-                localStorage.setItem('reload-url', window.location.href);
-            }
-        }
-
-        window.onload = function (e) {
-        }
-        const funcs = [setLayer1, setLayer2, setLayer3, setLayer4, setLayer5]; //functions are defined in ./scripts/loading.js, 
-
-        let scene = this.el;
-
-        function sleep(ms)
-        {
-            return new Promise(resolve => setTimeout(resolve, ms));
-        }
-
-
-        async function cacheCubeMaps () {
-            for (var i = 0; i < funcs.length; i++) {
-                funcs[i]();
-                await sleep(1);
-            }
-        }
-
-
-      /!*  function cacheCubeMap () {
-             cubemap[i].visible = true;
-             i ++
-            }
-        *!/
-
-        scene.addEventListener('loadnext', ()=> cacheCubeMaps)
-        scene.addEventListener('loaded', function () {
-
-
-            if (localStorage.getItem('reload-url') != null)
-             {
-                 if (window.location.href == localStorage.getItem('reload-url'));
-                 {
-                   
-                     console.log("revisiting"); // load without intro
-                 }
-             }
-
-
-
-            cacheCubeMaps();
-            revisting = true;
-            console.log("cached!")
-            
-        });
-    },
-}); */
--- observer.js
+++ observer.js
@@ -1,10 +1,10 @@
 
 AFRAME.registerComponent("observe", {
-    schema: {type: 'string', default: "na"},
+    //schema: {type: 'string', default: "na"},
     init: function () {
-        let data = this.data;
+      //  let data = this.data;
         const el = this.el;
-        console.log(data);
+        //console.log(data);
         //this.el.addEventListener("loaded", ()=> console.log("loaded"));
         el.addEventListener("componentchanged", onchange);
 
@@ -21,45 +21,14 @@ AFRAME.registerComponent("observe", {
         }
         
          async function onchange(){
-            console.log(data + " has changed");
+          //  console.log(data + " has changed");
              el.removeEventListener("componentchanged", onchange); // S
 
              await sleep(10);
             loadNext();
 
-            // el.removeEventListener("componentchanged", onchange,  true);
         }
-
-
-     
-     
-        /*
-                const targetNode = this.el;
         
-        
-        // Options for the observer (which mutations to observe)
-                const config = { attributes: true, childList: true, subtree: true };
-        
-        // Callback function to execute when mutations are observed
-                const callback = (mutationList, observer) => {
-                    for (const mutation of mutationList) {
-                        if (mutation.type === "childList") {
-                            console.log("A child node has been added or removed.");
-                        } else if (mutation.type === "attributes") {
-                            console.log(`The ${mutation.attributeName} attribute was modified.`);
-                        }
-                    }
-                };
-        
-        // Create an observer instance linked to the callback function
-                const observer = new MutationObserver(callback);
-        
-        // Start observing the target node for configured mutations
-                observer.observe(targetNode, config);
-        
-        // Later, you can stop observing
-        //observer.disconnect();
-        */
 
 
 
