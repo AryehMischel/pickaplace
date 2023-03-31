62c78962835e43c821c6fba0957bff12ef991714
Aryeh Mischel <asmischel5@gmail.com>
2023-03-31 16:05:46 -0400

    printing component change

--- observer.js
+++ observer.js
@@ -4,33 +4,35 @@ AFRAME.registerComponent("observe", {
         
         console.log("loading")
         this.el.addEventListener("loaded", ()=> console.log("loaded"));
-/*
-        const targetNode = this.el;
+        this.el.addEventListener("componentchanged", ()=> console.log(this.el + " has changed"));
 
-
-// Options for the observer (which mutations to observe)
-        const config = { attributes: true, childList: true, subtree: true };
-
-// Callback function to execute when mutations are observed
-        const callback = (mutationList, observer) => {
-            for (const mutation of mutationList) {
-                if (mutation.type === "childList") {
-                    console.log("A child node has been added or removed.");
-                } else if (mutation.type === "attributes") {
-                    console.log(`The ${mutation.attributeName} attribute was modified.`);
-                }
-            }
-        };
-
-// Create an observer instance linked to the callback function
-        const observer = new MutationObserver(callback);
-
-// Start observing the target node for configured mutations
-        observer.observe(targetNode, config);
-
-// Later, you can stop observing
-//observer.disconnect();
-*/
+        /*
+                const targetNode = this.el;
+        
+        
+        // Options for the observer (which mutations to observe)
+                const config = { attributes: true, childList: true, subtree: true };
+        
+        // Callback function to execute when mutations are observed
+                const callback = (mutationList, observer) => {
+                    for (const mutation of mutationList) {
+                        if (mutation.type === "childList") {
+                            console.log("A child node has been added or removed.");
+                        } else if (mutation.type === "attributes") {
+                            console.log(`The ${mutation.attributeName} attribute was modified.`);
+                        }
+                    }
+                };
+        
+        // Create an observer instance linked to the callback function
+                const observer = new MutationObserver(callback);
+        
+        // Start observing the target node for configured mutations
+                observer.observe(targetNode, config);
+        
+        // Later, you can stop observing
+        //observer.disconnect();
+        */
 
 
 
